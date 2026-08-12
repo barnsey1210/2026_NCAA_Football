d9325b99e71dd76603e1ab632ea7f5d39cd0a93b
barnsey1210 <jlindesmith@gmail.com>
2026-08-12 02:06:47 -0400

    Daily NCAAF data update 2026-08-12

--- matchup_workspace.js
+++ matchup_workspace.js
@@ -1,10 +1,10 @@
 (function () {
   'use strict';
 
-  const DATA_URL = 'data/site/matchups_view.json?v=20260812T052751Z';
+  const DATA_URL = 'data/site/matchups_view.json?v=20260812T060507Z';
   const HISTORY_URL = (
     window.MATCHUP_LINE_HISTORY_URL
-    || `data/site/matchup_line_history.json?v=20260812T052751Z)}`
+    || `data/site/matchup_line_history.json?v=20260812T060507Z)}`
   );
   const DECISION_KEY = 'openers-v2-decisions';
   const BET_KEY = 'ncaaf-game-bets-v1';
