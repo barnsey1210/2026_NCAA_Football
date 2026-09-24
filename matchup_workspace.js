af431958e35b05bf04d324fae355f028a8ab01e9
barnsey1210 <jlindesmith@gmail.com>
2026-09-23 20:35:46 -0400

    Daily NCAAF data update 2026-09-23

--- matchup_workspace.js
+++ matchup_workspace.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  const DATA_URL = 'data/site/matchups_public_view.json?v=20260923T141138Z';
+  const DATA_URL = 'data/site/matchups_public_view.json?v=20260924T003502Z';
   const HISTORY_URL = (
     window.MATCHUP_LINE_HISTORY_URL
     || `data/site/matchup_line_history.json?v=${Date.now()}`
